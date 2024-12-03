5dadd1ef6baed1c50ecfb9d5aa9a0c59c99e999d
esraa_ehab <esraaehab530@gmail.com>
2024-12-03 10:38:44 +0200

    Initialize Repository

---
+++ android/app/src/main/kotlin/com/example/fci_edutrack/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fci_edutrack
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:fci_edutrack/screens/home_screen.dart';
+import 'package:fci_edutrack/screens/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "FCIT EduTrack",
+      debugShowCheckedModeBanner: false,
+      initialRoute: SplashScreen.routeName,
+      routes: {
+        SplashScreen.routeName: (context) => const SplashScreen(),
+        HomeScreen.routeName: (context) => const HomeScreen(),
+      },
+    );
+  }
+}
