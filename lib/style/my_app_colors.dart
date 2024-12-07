ee95c09d6789154e3e8baf7b440ae895d514809a
esraa_ehab <esraaehab530@gmail.com>
2024-12-07 07:31:16 +0200

    finished animated splash screen

--- lib/style/my_app_colors.dart
+++ lib/style/my_app_colors.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 class MyAppColors {
-  static const Color blueColor = Color(0xff2702CB);
+  static const Color primaryColor = Color(0xff0066CC);
+  static const Color darkBlueColor = Color(0xff041C2D);
   static const Color lightBlueColor = Color(0xff2382C6);
   static const Color whiteColor = Color(0xffffffff);
 }
