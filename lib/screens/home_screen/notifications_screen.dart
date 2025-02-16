379e60143bc11e40518041d4703e50840b485181
esraa_ehab <esraaehab530@gmail.com>
2025-02-16 21:49:01 +0200

    make the notifications screen scrollable

--- lib/screens/home_screen/notifications_screen.dart
+++ lib/screens/home_screen/notifications_screen.dart
@@ -6,21 +6,26 @@ class NotificationsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(MediaQuery.of(context).size.width * 0.1),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset('assets/images/notification_logo.png',
-              width: MediaQuery.of(context).size.width * 0.8),
-          SizedBox(
-            height: MediaQuery.of(context).size.width * 0.1,
-          ),
-          Text(
-            'No Notifications Found',
-            style: MyThemeData.lightModeStyle.textTheme.titleMedium,
-          ),
-        ],
+    return SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.all(MediaQuery.of(context).size.width * 0.1),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.width * 0.3,
+            ),
+            Image.asset('assets/images/notification_logo.png',
+                width: MediaQuery.of(context).size.width * 0.8),
+            SizedBox(
+              height: MediaQuery.of(context).size.width * 0.1,
+            ),
+            Text(
+              'No Notifications Found',
+              style: MyThemeData.lightModeStyle.textTheme.titleMedium,
+            ),
+          ],
+        ),
       ),
     );
   }
